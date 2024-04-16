4f3f91ad4ab721adf79f81ce6c95a9766f5530f7
Edward <edward_job@163.com>
2024-04-16 16:18:50 +0800

    chore: 添加 git commit 校验 lintstaged husky commitlint

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <view class="content">
+  <view class="content center flex-col">
     <image class="logo" src="/static/logo.png" />
     <view class="text-area">
       <text class="title">{{ title }}</text>
@@ -15,20 +15,10 @@ const title = ref('Hello')
 </script>
 
 <style>
-.content {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-}
-
 .logo {
-  height: 200rpx;
   width: 200rpx;
-  margin-top: 200rpx;
-  margin-left: auto;
-  margin-right: auto;
-  margin-bottom: 50rpx;
+  height: 200rpx;
+  margin: 200rpx auto 50rpx;
 }
 
 .text-area {
