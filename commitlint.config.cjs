f04567ae50acff87d5155d035f130ba3f0d67db5
Edward <edward_job@163.com>
2024-04-16 17:15:35 +0800

    chore: commitlint 配置修改

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -3,13 +3,6 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    // 'body-leading-blank': [2, 'always'], // 主体前有空行，默认就是 always
-    // 'footer-leading-blank': [2, 'always'], // 末行前有空行，默认就是 always
-    // 'header-max-length': [2, 'always', 108], // 首行最大长度，默认就是 always,72
-    // 'subject-empty': [2, 'never'], // 标题不可为空，默认就是 never
-    // 'type-empty': [2, 'never'], // 类型不可为空，默认就是 never
-
-    // 允许的类型
     'type-enum': [
       2,
       'always',
@@ -25,27 +18,7 @@ module.exports = {
         'revert', // 回退
         'style', // 代码风格（不影响代码含义）
         'test', // 测试
-
-        // 下面几个是自定义新增的
-        'wip', // 开发中
-        'refine', // 小优化，没有到 refactor 的程度
       ],
     ],
   },
 }
-
-// @see https://commitlint.js.org/#/reference-rules?id=type-enum
-// 默认值为：
-// [
-//   'build',
-//   'chore',
-//   'ci',
-//   'docs',
-//   'feat',
-//   'fix',
-//   'perf',
-//   'refactor',
-//   'revert',
-//   'style',
-//   'test',
-// ];
